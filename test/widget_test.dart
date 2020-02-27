bbefea33b95c4026881992534b6b016f2d2f298a
luma-pinheiro <lumagabrielypinheiro@gmail.com>
2020-02-27 10:34:59 -0300

    primeiro commit

---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Center(
+    child: Container(
+        color: Colors.blue,
+        child: Text("Ado ado ado quem ta lendo é viado",
+            style: TextStyle(color: Colors.black))),
+  ),
+));
